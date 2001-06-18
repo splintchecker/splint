3811eaa011b6352a4c2391e1c8e8b06b2adf78d6
dlaroche <dlaroche>
2001-06-18 13:18:38 +0000

    Changed to used /\ and requires instead of bufferConstraint changed to used /\ and requires and ensures instead of bufferConstraint and ensuresConstraint

--- test/globalbufferannotation/globalvariable.c
+++ test/globalbufferannotation/globalvariable.c
@@ -3,10 +3,10 @@
 static char f [12];
 extern void /*@alt char * @*/
   mstrcpy (/*@unique@*/ /*@out@*/ /*returned*/ char *s1, char *s2) 
-     /*@modifies *s1@*/ /*@bufferConstraint MaxSet(s1) >= MaxRead(s2); @*/;
+     /*@modifies *s1@*/ /*@requires MaxSet(s1) >= MaxRead(s2); @*/;
 
 
-void copyf (char * c) /*@bufferConstraint MaxSet(c) >= MaxRead(f); @*/
+void copyf (char * c) /*@requires MaxSet(c) >= MaxRead(f); @*/
 {
   mstrcpy (c, f);
 }
