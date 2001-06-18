ff573bbd490233226f9c1d211e4e85a5c3ced512
dlaroche <dlaroche>
2001-06-18 18:36:16 +0000

    Added check of user specified post conditions. Added the flag +checkpost ot turn this on and off.

--- src/Headers/local_constants.h
+++ src/Headers/local_constants.h
@@ -1,7 +1,7 @@
 /* local_constants.h - created automatically by gmake localconstants */
 /*@constant observer char *SYSTEM_LIBDIR;@*/
-# define SYSTEM_LIBDIR /usr/include
+# define SYSTEM_LIBDIR "/usr/include"
 /*@constant observer char *DEFAULT_LARCHPATH;@*/
-# define DEFAULT_LARCHPATH /usr/local/lclint-2.5m/lib
+# define DEFAULT_LARCHPATH ".:/afs/cert.org/usr/dlaroche/LCLintDev/lib"
 /*@constant observer char *DEFAULT_LCLIMPORTDIR;@*/
-# define DEFAULT_LCLIMPORTDIR /usr/local/lclint-2.5m/imports
+# define DEFAULT_LCLIMPORTDIR "/afs/cert.org/usr/dlaroche/LCLintDev/imports"
