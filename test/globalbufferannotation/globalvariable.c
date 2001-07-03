27e60f784c9ec06d528cb4da9d7e78d886d67509
dlaroche <dlaroche>
2001-07-03 18:50:04 +0000

    Added some new test cases also merged with Dave Evans's tests.

--- test/globalbufferannotation/globalvariable.c
+++ test/globalbufferannotation/globalvariable.c
@@ -3,10 +3,10 @@
 static char f [12];
 extern void /*@alt char * @*/
   mstrcpy (/*@unique@*/ /*@out@*/ /*returned*/ char *s1, char *s2) 
-     /*@modifies *s1@*/ /*@requires MaxSet(s1) >= MaxRead(s2); @*/;
+     /*@modifies *s1@*/ /*@LRequires MaxSet(s1) >= MaxRead(s2); @*/;
 
 
-void copyf (char * c) /*@requires MaxSet(c) >= MaxRead(f); @*/
+void copyf (char * c) /*@LRequires MaxSet(c) >= MaxRead(f); @*/
 {
   mstrcpy (c, f);
 }
