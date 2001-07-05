af9afcab4f9342abfed15dcc6ff0172c7f268446
drl7x <drl7x>
2001-07-05 21:31:52 +0000

    Added basic support for switch statements Also added support the simplifications rules like:  x < y => x - 2 < y This is useful for resolving constraints.

--- src/Headers/herald.h
+++ src/Headers/herald.h
@@ -4,4 +4,4 @@
 /*@constant observer char *LCL_PARSE_VERSION;@*/
 # define LCL_PARSE_VERSION "LCLint 3.0b-alpha"
 /*@constant observer char *LCL_COMPILE;@*/
-# define LCL_COMPILE "Compiled using gcc -Wall -g on Linux paisley 2.4.3-12 #1 Fri Jun 8 13:35:30 EDT 2001 i686 unknown by evans"
+# define LCL_COMPILE "Compiled using gcc -DSTDC_HEADERS=1 -g -Wall on Linux fowler 2.4.3-12 #1 Fri Jun 8 13:35:30 EDT 2001 i686 unknown by drl7x"
