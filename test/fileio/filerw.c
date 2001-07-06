f14df15cbd163fa22e6a99825d3fd7f43e5b6560
evans <evans>
2001-07-06 02:55:03 +0000

    *** empty log message ***

--- test/fileio/filerw.c
+++ test/fileio/filerw.c
@@ -1,11 +1,14 @@
 int f (/*@open@*/ /*@read@*/ FILE *f1, /*@open@*/ /*@write@*/ FILE *f2)
 {
-  fgetc (f1);
-  fgetc (f2); /* error: not readable */
-  
-  fputc ('a', f1);
-  fseek (f1, 0, SEEK_CUR); 
-  fputc ('a', f1);
+  int c;
 
-  fgetc (f1); /* error */
+  c = fgetc (f1);
+  c = fgetc (f2); /* error: not readable */
+  
+  (void) fputc ('a', f1); /* error */
+  (void) fseek (f1, 0, SEEK_CUR); 
+  (void) fputc ('a', f1); /* okay */
+ 
+  c = fgetc (f1); /* error */
+  return c;
 }
