7e8563354b3ee8b2e0130c2e3785540b601855e9
drl7x <drl7x>
2001-07-16 18:49:00 +0000

    *** empty log message ***

--- test/moreBufferTests/strrchr.c
+++ test/moreBufferTests/strrchr.c
@@ -1,13 +1,8 @@
 
-extern /*@null@*/ /*@exposed@*/ char *
-strrchr (/*@returned@*/ char *s, int /*@alt char@*/ c)     /*@*/  /*@ensures MaxSet(result) >= 0 /\ MaxSet(result) <= MaxSet(s) /\ MaxRead (result) <= MaxRead(s) /\ MaxRead(result) >= 0 @*/ ;
-
 void
 bar (char * t)
 {
 
-  char *ot = t;
-  
   char *tt;
   tt = strrchr (t, '\n');
 }
