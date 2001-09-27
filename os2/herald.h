26e97ae300568e20e8e55e1c5e3171fe3922bc09
herbert <herbert>
2001-09-27 10:11:29 +0000

    version

--- os2/herald.h
+++ os2/herald.h
@@ -1,7 +1,7 @@
 /* herald.h - created automatically from herald.os2 and Makefile.os2 */
 /*@constant observer char *LCL_VERSION;@*/
-# define LCL_VERSION "LCLint 3.0.0.15 --- Sep 11 2001"
+# define LCL_VERSION "LCLint 3.0.0.16 --- Sep 25 2001"
 /*@constant observer char *LCL_PARSE_VERSION;@*/
-# define LCL_PARSE_VERSION "LCLint 3.0.0.15"
+# define LCL_PARSE_VERSION "LCLint 3.0.0.16"
 /*@constant observer char *LCL_COMPILE;@*/
-# define LCL_COMPILE "Compiled using icc -q -W2 -Dunlink=unlink -O+ -G5 -Gf+ -Gi+ -Gs+ \n on OS/2 Tue Sep 11 16:55:42 CET 2001 i586 by herbert,\n OS/2 specific subversion is 2"
+# define LCL_COMPILE "Compiled using icc -q -W2 -Dunlink=unlink -O+ -G5 -Gf+ -Gi+ -Gs+ \n on OS/2 Tue Sep 25 16:55:42 CET 2001 i586 by herbert,\n OS/2 specific subversion is 2"
