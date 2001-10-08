d52778ab03edab332145dc9cdc568f2af3458833
drl7x <drl7x>
2001-10-08 22:54:59 +0000

    Fixed Makefile to create bin directory.

--- src/Headers/local_constants.h
+++
@@ -1,7 +0,0 @@
-/* local_constants.h - created automatically by gmake localconstants */
-/*@constant observer char *SYSTEM_LIBDIR;@*/
-# define SYSTEM_LIBDIR "/usr/include"
-/*@constant observer char *DEFAULT_LARCHPATH;@*/
-# define DEFAULT_LARCHPATH "/usr/local/lclint/lib"
-/*@constant observer char *DEFAULT_LCLIMPORTDIR;@*/
-# define DEFAULT_LCLIMPORTDIR "/usr/local/lclint/imports"
