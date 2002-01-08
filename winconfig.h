09a1e6d2007076d0ccf2819eb983a8ccb65cc5fd
evans1629 <evans1629>
2002-01-08 19:57:04 +0000

    Added winconfig.h

---
+++ winconfig.h
@@ -0,0 +1,40 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.hin.  Generated automatically from configure.ac by autoheader.  */
+
+/*@-constmacros@*/
+
+/* The directory where Splint's library files live */
+#define DEFAULT_LARCHPATH ".;C:/splint-3.0.1/lib"
+
+/* Splint's imports directory */
+#define DEFAULT_LCLIMPORTDIR ".;C:/splint-3.0.1/imports"
+
+/* system include directory */
+#define GCC_INCLUDE_DIR "C:/include"
+
+/* alternate include directory */
+#define GCC_INCLUDE_DIR2 "C:/local/include"
+
+/* String describing who compiled this binary and how */
+#define LCL_COMPILE "Compiled using Microsoft Visual C++ by evans"
+
+/* Splint's version number */
+#define LCL_PARSE_VERSION "Splint 3.0.1"
+
+/* Define to disable support LCL files */
+/* #undef NOLCL */
+
+/* Splint's version number and configure/build date */
+#define SPLINT_VERSION "Splint 3.0.1 --- 07 Jan 2002"
+
+/* The system's main include directory */
+#define SYSTEM_LIBDIR "C:/include"
+
+/* Define if you're on a Unixy system */
+/* !!! #define UNIX 1 */
+
+/* Define if `lex' declares `yytext' as a `char *' by default, not a `char[]'.
+   */
+#define YYTEXT_POINTER 1
+
+/*@=constmacros@*/
