5ae275bf84b888cebe09e6fa4622de79d80b7f92
drl7x <drl7x>
2003-03-02 19:27:25 +0000

    Fixed data on readme. Also fixed some problems in the scripts to create binary tar balls.

--- fixBinaryDist.sh
+++ fixBinaryDist.sh
@@ -10,15 +10,20 @@ pwd
 #Need to do this so modifications to the destination directory won't effect
 #the real directory 
 rm Makefile.am
-rm configure.ac 
-cp  Makefile.binary.am Makefile.am
-cp  configure.binary.ac configure.ac
-cp configure.binary configure
-cp Makefile.binary.in Makefile.in
+rm configure.ac
 
+mv  Makefile.binary.am Makefile.am
+mv  configure.binary.ac configure.ac
+sleep 3
+mv configure.binary configure
+
+
+#should probably do this in Makefile but it doesn't seem to work..
+sed -e 's/Makefile\.binary/Makefile/' Makefile.binary.in  > Makefile.in
+rm Makefile.binary.in
 #run these if possible...
-autoconf
-automake
+#autoconf
+#automake
 mkdir src || echo "COULD NOT MAKE src tar ball may be incorrect"
 echo "Created this file because some versions of tar are too simple include this directory without it"  > src/dummy
 
